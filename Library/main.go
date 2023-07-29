73d2590dbb62e182b0cf74bce19d29912b778151
Sergey Ryazanov <majo78@mail.ru>
2023-07-29 18:14:30 +0300

    Add connect for the db

--- Library/main.go
+++ Library/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
 func main() {
-	println("Hello, world!")
+	db, err := sql.Open("mysql", "root:Kvado2023@tcp(0.0.0.0:3306)/librarydb")
+	if err != nil {
+		fmt.Println("Failed to connect to database:", err)
+		return
+	}
+	defer db.Close()
+
+	fmt.Println("Book inserted successfully!")
 }
