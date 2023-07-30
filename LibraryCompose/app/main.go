403b4553a8ed4ed512e0fdecbfae7d85589df023
Sergey Ryazanov <majo78@mail.ru>
2023-07-31 02:24:48 +0300

    Successful connection to the database

--- LibraryCompose/app/main.go
+++ LibraryCompose/app/main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	db, err := sql.Open("mysql", "myuser:mypassword@tcp(db:3306)/mydb")
+	db, err := sql.Open("mysql", "myuser:mypassword@tcp(172.24.0.1:3306)/mydb")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -23,4 +23,9 @@ func main() {
 	fmt.Println("Successfully connected to the database!")
 
 	// Здесь вы можете добавить свой код для управления базой данных
+
+	err = db.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
