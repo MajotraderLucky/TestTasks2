3e7adbc34f75ed00db070a6cdb144fdad2df2654
Sergey Ryazanov <majo78@mail.ru>
2023-07-31 06:56:53 +0300

    Add protobuf

--- LibraryCompose/app/main.go
+++ LibraryCompose/app/main.go
@@ -1,18 +1,70 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"log"
-	"os"
-	"path/filepath"
-	"time"
+	"net"
 
 	_ "github.com/go-sql-driver/mysql"
+	"google.golang.org/grpc"
 )
 
+const (
+	port = ":50051"
+)
+
+type server struct {
+	db *sql.DB
+}
+
+func (s *server) FindBooksByAuthor(ctx context.Context, req *pb.AuthorRequest) (*pb.BooksResponse, error) {
+	rows, err := s.db.Query("SELECT book_name FROM books WHERE author_name = ?", req.AuthorName)
+	if err != nil {
+		log.Printf("Error querying books: %v", err)
+		return nil, err
+	}
+	defer rows.Close()
+
+	var books []string
+	for rows.Next() {
+		var book string
+		err := rows.Scan(&book)
+		if err != nil {
+			log.Printf("Error scanning book: %v", err)
+			continue
+		}
+		books = append(books, book)
+	}
+
+	return &pb.BooksResponse{Books: books}, nil
+}
+
+func (s *server) FindAuthorsByBook(ctx context.Context, req *pb.BookRequest) (*pb.AuthorsResponse, error) {
+	rows, err := s.db.Query("SELECT author_name FROM authors WHERE book_name = ?", req.BookName)
+	if err != nil {
+		log.Printf("Error querying authors: %v", err)
+		return nil, err
+	}
+	defer rows.Close()
+
+	var authors []string
+	for rows.Next() {
+		var author string
+		err := rows.Scan(&author)
+		if err != nil {
+			log.Printf("Error scanning author: %v", err)
+			continue
+		}
+		authors = append(authors, author)
+	}
+
+	return &pb.AuthorsResponse{Authors: authors}, nil
+}
+
 func main() {
-	db, err := sql.Open("mysql", "myuser:mypassword@tcp(172.24.0.1:3306)/mydb")
+	db, err := sql.Open("mysql", "myuser:mypassword@tcp(172.24.0.2:3306)/mydb")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -25,38 +77,13 @@ func main() {
 
 	fmt.Println("Successfully connected to the database!")
 
-	// Создание таблицы authors
-	createAuthorsTable :=
-		`CREATE TABLE IF NOT EXISTS authors (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL);`
-
-	_, err = db.Exec(createAuthorsTable)
+	lis, err := net.Listen("tcp", port)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("failed to listen: %v", err)
 	}
-
-	fmt.Println("Create table autors successfully")
-
-	err = db.Close()
-	if err != nil {
-		log.Fatal(err)
+	s := grpc.NewServer()
+	pb.RegisterLibraryServiceServer(s, &server{db: db})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
 	}
-
-	logDir := "/home/ryazanov/Development/TestTasks/LibraryCompose/logs"
-	err = os.MkdirAll(logDir, os.ModePerm)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	logFile := filepath.Join(logDir, time.Now().Format("20060102_150405")+".log")
-	file, err := os.OpenFile(logFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
-	log.SetOutput(file)
-	log.Println("Successfully connected to the database!")
-	log.Println("Create table authors successfully")
 }
---
+++ Repo/protobuf/protobuf.go
@@ -0,0 +1,15 @@
+package protobuf
+
+import (
+	"fmt"
+)
+
+// Function1 is a function in the pb package.
+func Function1() {
+	fmt.Println("Hello from Function1 in pb package")
+}
+
+// Function2 is another function in the pb package.
+func Function2() {
+	fmt.Println("Hello from Function2 in pb package")
+}
