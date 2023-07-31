7c01ddf97c9f139972d942668b3e43e14a53bdcc
Sergey Ryazanov <majo78@mail.ru>
2023-08-01 01:34:11 +0300

    Add check status db

--- ApiGrpcLibrary/app/main.go
+++ ApiGrpcLibrary/app/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
@@ -17,10 +20,38 @@ func main() {
 	}
 
 	// Создание файла log.txt и запись в него текста
-	err = ioutil.WriteFile("logs/log.txt", []byte("Hello, logs!"), 0644)
+	err = ioutil.WriteFile("logs/log.txt", []byte("Hello, logs!\n"), 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	fmt.Println("File log.txt created and text written successfully.")
+
+	db, err := sql.Open("mysql", "myuser:mypassword@tcp(db:3306)/mydb")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Чтение содержимого файла log.txt
+	content, err := ioutil.ReadFile("logs/log.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Добавление новой строки к содержимому файла
+	content = append(content, []byte("Successfully connected to the database!\n")...)
+
+	// Запись обновленного содержимого обратно в файл
+	err = ioutil.WriteFile("logs/log.txt", content, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Successfully connected to the database!")
 }
