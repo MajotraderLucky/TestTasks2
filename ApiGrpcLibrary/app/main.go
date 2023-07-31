a1c2ecc241ad52bffa732d829d97586dc4096de3
Sergey Ryazanov <majo78@mail.ru>
2023-08-01 00:08:59 +0300

    Add logs

--- ApiGrpcLibrary/app/main.go
+++ ApiGrpcLibrary/app/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+)
 
 func main() {
 	fmt.Println("Hello, world!")
+
+	// Создание директории logs, если она не существует
+	err := os.MkdirAll("logs", 0755)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Создание файла log.txt и запись в него текста
+	err = ioutil.WriteFile("logs/log.txt", []byte("Hello, logs!"), 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("File log.txt created and text written successfully.")
 }
