7673c2069a93e0e0528b52d2a9676bed7f1daeaf
Sergey Ryazanov <majo78@mail.ru>
2023-07-31 05:14:41 +0300

    Add volume - local dir for the logs

--- LibraryCompose/app/main.go
+++ LibraryCompose/app/main.go
@@ -4,6 +4,9 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
+	"path/filepath"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -22,10 +25,38 @@ func main() {
 
 	fmt.Println("Successfully connected to the database!")
 
-	// Здесь вы можете добавить свой код для управления базой данных
+	// Создание таблицы authors
+	createAuthorsTable :=
+		`CREATE TABLE IF NOT EXISTS authors (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL);`
+
+	_, err = db.Exec(createAuthorsTable)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Create table autors successfully")
 
 	err = db.Close()
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	logDir := "/home/ryazanov/Development/TestTasks/LibraryCompose/logs"
+	err = os.MkdirAll(logDir, os.ModePerm)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	logFile := filepath.Join(logDir, time.Now().Format("20060102_150405")+".log")
+	file, err := os.OpenFile(logFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	log.SetOutput(file)
+	log.Println("Successfully connected to the database!")
+	log.Println("Create table authors successfully")
 }
