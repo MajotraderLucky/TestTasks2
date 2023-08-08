6a778e5cad6f5ee33d01601986332408a8c69603
MajotraderLucky <majohead@gmail.com>
2023-08-08 07:31:19 +0300

    Add protobuf

--- ApiGrpcLibrary/servergrpc/server.go
+++ ApiGrpcLibrary/servergrpc/server.go
@@ -1,9 +1,33 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"log"
+	"net"
 	"os"
+
+	"google.golang.org/grpc"
 )
+
+type server struct{}
+
+func (s *server) Ping(ctx context.Context, request *PingRequest) (*PingResponse, error) {
+	// Подключение к базе данных
+	db, err := sql.Open("mysql", "myuser:mypassword@tcp(db:3306)/mydb")
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+	defer db.Close()
+
+	// Пинг базы данных
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("Failed to ping database: %v", err)
+	}
+
+	return &PingResponse{Message: "Ping successful"}, nil
+}
 
 func createLogsDirectory() error {
 	err := os.MkdirAll("logs", 0755)
@@ -40,4 +64,17 @@ func main() {
 
 	// Запись в лог
 	log.Println("Hello, GRPC Server!")
+
+	// Создание gRPC сервера
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	RegisterPingServiceServer(s, &server{})
+
+	// Запуск gRPC сервера
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
 }
