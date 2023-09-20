8bed13dd26340dfe25a884326f4042788508250d
MajotraderLucky <majohead@gmail.com>
2023-09-20 06:07:31 +0300

    feat(server): Add a GetData function and connect to the database

--- ApiGrpcLibrary/findbookgrpc/servergrpc/go.mod
+++ ApiGrpcLibrary/findbookgrpc/servergrpc/go.mod
@@ -2,4 +2,15 @@ module servergrpc
 
 go 1.21
 
-require github.com/MajotraderLucky/Utils v0.0.0-20230831234517-5ffc888064d2 // indirect
+require (
+	github.com/MajotraderLucky/TestTasks2 v0.0.0-20230918035311-fd2a209995da // indirect
+	github.com/MajotraderLucky/Utils v0.0.0-20230831234517-5ffc888064d2 // indirect
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	golang.org/x/net v0.12.0 // indirect
+	golang.org/x/sys v0.10.0 // indirect
+	golang.org/x/text v0.11.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230711160842-782d3b101e98 // indirect
+	google.golang.org/grpc v1.58.1 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+)
--- ApiGrpcLibrary/findbookgrpc/servergrpc/server.go
+++ ApiGrpcLibrary/findbookgrpc/servergrpc/server.go
@@ -1,10 +1,54 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"log"
 
+	pb "github.com/MajotraderLucky/TestTasks2/Repo/protobuf"
 	"github.com/MajotraderLucky/Utils/logger"
+	_ "github.com/go-sql-driver/mysql"
 )
+
+type MyQueryServiceServer struct {
+	pb.QueryServiceServer
+}
+
+func (s *MyQueryServiceServer) GetData(ctx context.Context, request *pb.GetDataRequest) (*pb.GetDataResponse, error) {
+	// Open a connection to the database.
+	db, err := sql.Open("mysql", "myuser:mypassword@tcp(db:3306)/mydb")
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+
+	// Get table names from the database and write them to the response.
+	logger := logger.Logger{}
+	logger.LogLine()
+	log.Println("Getting table names from the database...")
+	rows, err := db.Query("SHOW TABLES")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	var tableNames []string
+	for rows.Next() {
+		var tableName string
+		if err := rows.Scan(&tableName); err != nil {
+			log.Fatal(err)
+		}
+		tableNames = append(tableNames, tableName)
+	}
+	logger.LogLine()
+	log.Println("Got table names:", tableNames)
+
+	// Create and return the response.
+	response := &pb.GetDataResponse{
+		TableNames: tableNames,
+	}
+	return response, nil
+}
 
 func main() {
 	// Connect the logger package and make an entry in the log.
@@ -21,4 +65,11 @@ func main() {
 	logger.LogLine()
 
 	log.Println("Hello, Server gRPC!")
+
+	// Connect to the database.
+	db, err := sql.Open("mysql", "myuser:mypassword@tcp(db:3306)/mydb")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 }
